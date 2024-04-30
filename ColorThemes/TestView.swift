44da48af632d031472e868273d87bd0d198b39f8
Uhl Albert <bikrrr@gmail.com>
2024-04-30 10:18:38 -0600

    Added full set of coors for Rosé, Indigo, and Light themes. Created LazyVGrid of swatches to preview.

--- ColorThemes/TestView.swift
+++ ColorThemes/TestView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 public enum ThemeIdentifier: String, CaseIterable, Identifiable {
     case indigo = "Indigo"
     case rose = "Rosé"
+    case light = "Light"
 
     public var id: Self { self }
 }
@@ -27,12 +28,31 @@ struct UniversalColorTheme: ColorTheme {
     }
 }
 
-enum ColorName: String {
-    case primaryIcon01,
+enum ColorName: String, CaseIterable, Identifiable {
+    case primaryField01,
+         primaryField01Active,
+         primaryField02,
+         primaryField02Active,
+         primaryField03,
+         primaryField03Active,
+         primaryIcon01,
          primaryIcon01Active,
          primaryIcon02,
          primaryIcon02Active,
          primaryIcon02Selected,
+         primaryIcon03,
+         primaryIcon03Active,
+         primaryInteractive01,
+         primaryInteractive01Active,
+         primaryInteractive01Disabled,
+         primaryInteractive01Hover,
+         primaryInteractive02,
+         primaryInteractive02Active,
+         primaryInteractive02Hover,
+         primaryInteractive03,
+         primaryText01,
+         primaryText02,
+         primaryText02Selected,
          primaryUi01,
          primaryUi01Active,
          primaryUi02,
@@ -42,7 +62,20 @@ enum ColorName: String {
          primaryUi04,
          primaryUi05,
          primaryUi05Selected,
-         primaryUi06
+         primaryUi06,
+         secondaryField01,
+         secondaryField01Active,
+         secondaryIcon01,
+         secondaryIcon02,
+         secondaryInteractive01,
+         secondaryInteractive01Active,
+         secondaryInteractive01Hover,
+         secondaryText01,
+         secondaryText02,
+         secondaryUi01,
+         secondaryUi02
+
+    public var id: Self { self }
 }
 
 struct TestView: View {
@@ -52,9 +85,11 @@ struct TestView: View {
         UniversalColorTheme(identifier: selectedTheme)
     }
 
+    private let columns = Array(repeating: GridItem(.flexible(), spacing: 8), count: 5)
+
     var body: some View {
-        VStack {
-            Section("Page Theme") {
+        ScrollView {
+            Section("Theme") {
                 Picker("Theme", selection: $selectedTheme) {
                     ForEach(ThemeIdentifier.allCases, id: \.self) { theme in
                         Text(theme.rawValue).tag(theme)
@@ -63,20 +98,17 @@ struct TestView: View {
                 .pickerStyle(SegmentedPickerStyle())
             }
 
-            VStack {
-                Rectangle()
-                    .fill(theme.color(for: .primaryIcon02Selected))
-                    .frame(height: 100)
-
-                Rectangle()
-                    .fill(theme.color(for: .primaryUi05Selected))
-                    .frame(height: 100)
-
-                Rectangle()
-                    .fill(Color.red)
-                    .frame(height: 100)
+            Section("Swatches") {
+                LazyVGrid(columns: columns, spacing: 8) {
+                    ForEach(ColorName.allCases, id: \.self) { colorName in
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(theme.color(for: colorName))
+                            .aspectRatio(1, contentMode: .fit)
+                    }
+                }
             }
         }
+        .padding()
     }
 }
 
